d8ad0d1a4a33a631679b253fde208e5d25355534
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 14:42:58 -0800

    Siz is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@ printf("Size of a char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-printf("Size fo a float: %i byte(s)\n", (unsigned long)sizeof(float));
+printf("Size fo a float: %i byte(s)\n", sizeof(float));
+printf("Size of a double: %i byte(s)\n", sizeof(double));
 return (0);
 }
