2cd7fb083866f656381df982facac1373a2c1423
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 15:49:47 -0800

    Siz is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A Program to prints the size of variuos types of computer
+ * main - A Program to prints the size of variuos types of computer it is compiled and run on
  * Return: 0
  */
 int main(void)
@@ -9,6 +9,6 @@ printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size fo a float: %zu byte(s)\n", sizeof(float));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
