b9a1a6f1355739c625a589b8b0974ecc9e476567
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 09:32:47 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - A C program that print a simple sentence
+ * Return; 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
