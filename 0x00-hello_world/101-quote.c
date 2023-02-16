c382d1c7bd3641bac012483a9b843aaf1ef0a398
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 14:14:22 -0800

    UNIX is Basically a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-printf_s("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
