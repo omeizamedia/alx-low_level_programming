ca6c3fd8cf4196b0483067e3abcb8d7c20454e0c
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 14:53:08 -0800

    UNIX is Basically a simple OS

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,5 @@ printf("Size of an int: %i byte(s)\n", sizeof(int));
 printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size fo a float: %i byte(s)\n", sizeof(float));
-printf("Size of a double: %i byte(s)\n", sizeof(double));
 return (0);
 }
