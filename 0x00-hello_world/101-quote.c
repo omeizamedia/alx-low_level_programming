70088b6295fe992cfea5708ce0cf7a478c0b3555
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-16 14:19:07 -0800

    UNIX is Basically a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - A c program to prints a line followed by a new line
  * Return: 1
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
