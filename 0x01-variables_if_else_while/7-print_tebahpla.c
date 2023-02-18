c242ff63857b08fbd598b7043e965e8ac8c36c1c
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-02-17 18:06:12 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'a')
+		{
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
