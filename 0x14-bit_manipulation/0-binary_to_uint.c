8c1dbe0c2a02554b36acf59db425af315a95dd46
Ibrahim Omeiza <ibrahim_yahaya2020@yahoo.com>
2023-04-11 08:22:32 -0700

    task 100

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-#include <stddef.h>
-/**
- * binary_to_uint - convert a binary number to unsigned integer
- * written by Ibrahim Omeiza
- * @b: the pointer to store the numbers
- * Return: return 0 or num.
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int num = 0;
 
-	if (b == NULL)
-	return (0);
-	for (int i = 0; b[i] != '\0'; i++)
-	{
-	if (b[i] == '0')
-	{
-	num = (num << 1) | 0;
-	}
-	else if (b[i] == '1')
-	{
-	num = (num << 1) | 1;
-	}
-	else
-	{
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @z: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *z)
+{
+	int y;
+	unsigned int dv = 0;
+
+	if (!z)
 		return (0);
+
+	for (y = 0; z[y]; y++)
+	{
+		if (z[y] < '0' || z[y] > '1')
+			return (0);
+		dv = 2 * dv + (z[y] - '0');
 	}
-	}
-	return (num);
+
+	return (dv);
 }
